297f35bcde8ebc5490ba27854f2b6b699f567bd8
zariganitosh <XXXX@example.com>
2012-11-13 08:23:30 +0900

    Initial Commit

---
+++ dropshadow/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  dropshadow
+//
+//  Created by zarigani on 2012/11/13.
+//  Copyright (c) 2012年 bebe工房. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
