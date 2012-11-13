5302cebfdbc635ee14a98e6838afa3643b81e0d6
zariganitosh <XXXX@example.com>
2012-11-13 12:01:37 +0900

    複数ファイルに対応、オプション指定で影の設定を変更可能

--- dropshadow/main.m
+++ dropshadow/main.m
@@ -9,17 +9,18 @@
 #import <Cocoa/Cocoa.h>
 
 //影付きイメージを描画して返す
-NSImage* dropshadowImage(NSImage *image)
+NSImage* dropshadowImage(NSImage *image, float blurRadius, float alphaValue)
 {
+    float margin = blurRadius * 1.25;
     //スケールを取得する
     CGFloat scale = [[NSScreen mainScreen] backingScaleFactor];
     //Retina環境に応じたポイントサイズを取得する
     NSBitmapImageRep* imageRep = [NSBitmapImageRep imageRepWithData:[image TIFFRepresentation]];
-    NSSize pointSize = NSMakeSize([imageRep pixelsWide]/scale, [imageRep pixelsHigh]/scale);
+    NSSize dotSize = NSMakeSize([imageRep pixelsWide]/scale, [imageRep pixelsHigh]/scale);
     //描画する場所を準備
     NSRect newRect = NSZeroRect;
-    newRect.size.width = pointSize.width + 20;
-    newRect.size.height = pointSize.height + 20;
+    newRect.size.width = dotSize.width + margin*2;
+    newRect.size.height = dotSize.height + margin*2;
     NSImage *newImage = [[NSImage alloc] initWithSize:newRect.size];
     //描画する場所=newImageに狙いを定める、描画環境を保存しておく
     [newImage lockFocus];
@@ -28,14 +29,14 @@ NSImage* dropshadowImage(NSImage *image)
     [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
     //影の設定
     NSShadow *shadow = [[NSShadow alloc] init];
-    [shadow setShadowOffset:NSMakeSize(0.0, -2.0)];
-    [shadow setShadowBlurRadius:8];
-    [shadow setShadowColor:[[NSColor blackColor] colorWithAlphaComponent:0.5]];
+    [shadow setShadowOffset:NSMakeSize(0.0, -blurRadius * 0.25)];
+    [shadow setShadowBlurRadius:blurRadius];
+    [shadow setShadowColor:[[NSColor blackColor] colorWithAlphaComponent:alphaValue]];
     [shadow set];
     //描画する
     NSRect drawRect;
-    drawRect.origin = NSMakePoint(10, 10);
-    drawRect.size = pointSize;
+    drawRect.origin = NSMakePoint(margin, margin);
+    drawRect.size = dotSize;
     [image drawInRect:drawRect
              fromRect:NSZeroRect
             operation:NSCompositeSourceOver
@@ -45,7 +46,7 @@ NSImage* dropshadowImage(NSImage *image)
     //描画環境を元に戻す、描画する場所=newImageから狙いを外す
     [NSGraphicsContext restoreGraphicsState];
     [newImage unlockFocus];
-    //影付きのイメージを返す
+    
     return newImage;
 }
 
@@ -67,24 +68,53 @@ int main(int argc, char *argv[])
 {
 //    return NSApplicationMain(argc, (const char **)argv);
         
-    //ファイルパスをパースしておく
-//    NSString *aPath = @"~/a/b/c.d.e";
-    NSString *aPath = [NSString stringWithUTF8String:argv[1]];
-    NSString *fPath = [aPath stringByStandardizingPath];        // /Users/HOME/a/b/c.d.e
-    NSString *fDir = [fPath stringByDeletingLastPathComponent]; // /Users/HOME/a/b
-    NSString *fNameExt = [fPath lastPathComponent];             // c.d.e
-    NSString *fExt = [fPath pathExtension];                     // e
-    NSString *fDirName = [fPath stringByDeletingPathExtension]; // /Users/HOME/a/b/c.d
-//    NSLog(@"name.ext=%@  ext=%@  dir=%@  dir/name=%@", fNameExt, fExt, fDir, fDirName);
-    NSString *shadowPath = [[fDirName stringByAppendingString:@"-shadow."] stringByAppendingString:fExt];
+    int opt, i;
+    float blurRadius = 8.0;
+    float alphaValue = 0.5;
     
-    //影付きイメージを生成する
-    NSImage *image = [[NSImage alloc] initWithContentsOfFile:fPath];
-    NSImage *shadowImage = dropshadowImage(image);
-    saveImageByPNG(shadowImage, shadowPath);
+    //opterr = 0;/* エラーメッセージを非表示にする */
     
-    //画像情報を出力する
-    NSRect align = [shadowImage alignmentRect];
-    NSLog(@"%@ (%f, %f, %f, %f)", shadowPath, align.origin.x, align.origin.y, align.size.width, align.size.height);
+    while((opt = getopt(argc, argv, "a:b:")) != -1){
+        switch(opt){
+            case 'a':
+                sscanf(optarg, "%f", &alphaValue);
+                printf("  Option -%c = %f\n", opt, alphaValue);
+                break;
+                
+            case 'b':
+                sscanf(optarg, "%f", &blurRadius);
+                printf("  Option -%c = %f\n", opt, blurRadius);
+                break;
+                
+                // 解析できないオプションが見つかった場合は「?」を返す
+                // オプション引数が不足している場合も「?」を返す
+            case '?':
+                printf("Unknown or required argument option -%c\n", optopt);
+                printf("Usage: COMMAND [-m | -e] [-s suffix] name ...\n");
+                return 1;
+        }
+    }
+    
+    for(i = optind; i < argc; i++){
+        //ファイルパスをパースしておく
+//        NSString *aPath = @"~/a/b/c.d.e";
+        NSString *aPath = [NSString stringWithUTF8String:argv[i]];
+        NSString *fPath = [aPath stringByStandardizingPath];        // /Users/HOME/a/b/c.d.e
+        NSString *fDir = [fPath stringByDeletingLastPathComponent]; // /Users/HOME/a/b
+        NSString *fNameExt = [fPath lastPathComponent];             // c.d.e
+        NSString *fExt = [fPath pathExtension];                     // e
+        NSString *fDirName = [fPath stringByDeletingPathExtension]; // /Users/HOME/a/b/c.d
+//        NSLog(@"name.ext=%@  ext=%@  dir=%@  dir/name=%@", fNameExt, fExt, fDir, fDirName);
+        NSString *shadowPath = [[fDirName stringByAppendingString:@"-shadow."] stringByAppendingString:fExt];
+        
+        //影付きイメージを生成する
+        NSImage *image = [[NSImage alloc] initWithContentsOfFile:fPath];
+        NSImage *shadowImage = dropshadowImage(image, blurRadius, alphaValue);
+        saveImageByPNG(shadowImage, shadowPath);
+        
+        //画像情報を出力する
+        NSRect align = [shadowImage alignmentRect];
+        NSLog(@"%@ (%f, %f, %f, %f)", shadowPath, align.origin.x, align.origin.y, align.size.width, align.size.height);
+    }
     return 0;
 }
